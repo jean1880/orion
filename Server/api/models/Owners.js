07e34e891f59ecdb3d2e2a586fcc9d010d07b9b5
Jean-Luc Desroches <jean18801880@gmail.com>
2015-01-28 14:09:30 -0500

    add missing files

---
+++ Server/api/models/Owners.js
@@ -0,0 +1,20 @@
+/**
+ * Owners.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
+
+module.exports = {
+
+    attributes: {
+        Name: {
+            type: 'string',
+            require: true
+        },
+        Dogs: {
+            collection: 'dogs',
+            via: 'Owner'
+        }
+    }
+};
