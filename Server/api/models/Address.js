2920ac92ee254a8fef4296aa22f88b83183693b3
Jean-Luc Desroches <jean18801880@gmail.com>
2015-02-06 17:00:42 -0500

    store some DB fixes

--- Server/api/models/Address.js
+++ Server/api/models/Address.js
@@ -7,13 +7,33 @@
  * Refer to the ERD for more info:
  * https://editor.ponyorm.com/user/jean1880/DogTool
  * @class Address
-*/
+ */
 
 /*jslint node:true*/
 
 module.exports = {
 
     attributes: {
+        StreetName: {
+            type: 'string',
+            required: true
+        },
+        StreetNum: {
+            type: 'string',
+            required: true
+        },
+        City: {
+            type: 'string',
+            required: true
+        },
+        Province: {
+            type: 'string',
+            defaultsTo: 'ON'
+        },
+        Country: {
+            type: 'string',
+            defaultsTo: 'CA'
+        },
         business_infos: {
             collection: 'business_info',
             via: 'address'
