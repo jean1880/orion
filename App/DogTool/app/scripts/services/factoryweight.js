71894506cccd4891e3e80d51ea63b19c6833ec2a
Jean-Luc Desroches <jean18801880@gmail.com>
2015-02-18 17:20:20 -0500

    add comment on what to update

--- App/DogTool/app/scripts/services/factoryweight.js
+++ App/DogTool/app/scripts/services/factoryweight.js
@@ -1,5 +1,8 @@
 'use strict';
-
+/******************************* TODO:
+ * add @class with classname in top block quote, and an @author so someone
+ * looking at the code knows who to go to if they have questions
+ * *********************************/
 /**
  * @ngdoc service
  * @name dogToolApp.FactoryWeight
@@ -8,48 +11,48 @@
  * Factory in the dogToolApp.
  */
 angular.module('dogToolApp')
-  .factory('FactoryWeight', function ($sails) {
-    var route = '/Weight';
+    .factory('FactoryWeight', function ($sails) {
+        var route = '/Weight';
 
-    return {
-      /**
-       * One time fetch from server for single Weight dataset
-       * @method get
-       * @param id
-       */
-      get: function (id) {
-          return $sails.get(route + '/' + id);
-      },
-      /**
-       * One time fetch from server for full Weight dataset
-       * @method getAll
-       */
-      getAll: function () {
-          return $sails.get(route);
-      },
-      /**
-       * One time fetch from server for full Weight dataset
-       * @method listen
-       * @param callback
-       */
-      listen: function (callback) {
-          $sails.on(route, callback);
-      },
-      /**
-       * One time fetch from server for full Weight dataset
-       * @method post
-       * @param weight
-       */
-      post: function (weight) {
-          return $sails.post(route, weight);
-      },
-      /**
-       * One time fetch from server for full Weight dataset
-       * @method find
-       * @param searchObject
-       */
-      find: function (searchObject) {
-          return $sails.post(route + '/find', searchObject);
-      }
-    };
-  });
+        return {
+            /**
+             * One time fetch from server for single Weight dataset
+             * @method get
+             * @param id
+             */
+            get: function (id) {
+                return $sails.get(route + '/' + id);
+            },
+            /**
+             * One time fetch from server for full Weight dataset
+             * @method getAll
+             */
+            getAll: function () {
+                return $sails.get(route);
+            },
+            /**
+             * One time fetch from server for full Weight dataset
+             * @method listen
+             * @param callback
+             */
+            listen: function (callback) {
+                $sails.on(route, callback);
+            },
+            /**
+             * One time fetch from server for full Weight dataset
+             * @method post
+             * @param weight
+             */
+            post: function (weight) {
+                return $sails.post(route, weight);
+            },
+            /**
+             * One time fetch from server for full Weight dataset
+             * @method find
+             * @param searchObject
+             */
+            find: function (searchObject) {
+                return $sails.post(route + '/find', searchObject);
+            }
+        };
+    });
