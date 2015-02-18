72dba1160ab06e5712464fa4883e840c98c32375
Stephen A. Wilson <Stephen-356@hotmail.com>
2015-02-18 16:27:52 -0500

    write factory

--- App/DogTool/app/scripts/services/factoryweight.js
+++ App/DogTool/app/scripts/services/factoryweight.js
@@ -8,16 +8,48 @@
  * Factory in the dogToolApp.
  */
 angular.module('dogToolApp')
-  .factory('FactoryWeight', function () {
-    // Service logic
-    // ...
+  .factory('FactoryWeight', function ($sails) {
+    var route = '/Weight';
 
-    var meaningOfLife = 42;
-
-    // Public API here
     return {
-      someMethod: function () {
-        return meaningOfLife;
+      /**
+       * One time fetch from server for single Weight dataset
+       * @method get
+       * @param id
+       */
+      get: function (id) {
+          return $sails.get(route + '/' + id);
+      },
+      /**
+       * One time fetch from server for full Weight dataset
+       * @method getAll
+       */
+      getAll: function () {
+          return $sails.get(route);
+      },
+      /**
+       * One time fetch from server for full Weight dataset
+       * @method listen
+       * @param callback
+       */
+      listen: function (callback) {
+          $sails.on(route, callback);
+      },
+      /**
+       * One time fetch from server for full Weight dataset
+       * @method post
+       * @param weight
+       */
+      post: function (weight) {
+          return $sails.post(route, weight);
+      },
+      /**
+       * One time fetch from server for full Weight dataset
+       * @method find
+       * @param searchObject
+       */
+      find: function (searchObject) {
+          return $sails.post(route + '/find', searchObject);
       }
     };
   });
