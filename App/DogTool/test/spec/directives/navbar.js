8cd907a7ec4f239b828730eb30a7be98fc04cc27
Stephen A. Wilson <Stephen-356@hotmail.com>
2015-02-18 22:22:48 -0500

    delint tests for the navbar

--- App/DogTool/test/spec/directives/navbar.js
+++ App/DogTool/test/spec/directives/navbar.js
@@ -5,8 +5,7 @@ describe('Directive: navBar', function () {
   // load the directive's module
   beforeEach(module('dogToolApp'));
 
-  var element,
-    scope;
+  var scope;
 
   beforeEach(inject(function ($rootScope) {
     scope = $rootScope.$new();
