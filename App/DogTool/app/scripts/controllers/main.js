0a717112067f4a9e7c5efe80037acaf4fb1755d8
Jean-Luc Desroches <jean18801880@gmail.com>
2015-02-24 15:45:44 -0500

    fix conflicts

--- App/DogTool/app/scripts/controllers/main.js
+++ App/DogTool/app/scripts/controllers/main.js
@@ -8,17 +8,17 @@
  * Controller of the dogToolApp
  */
 angular.module('dogToolApp')
-  .controller('MainCtrl', function ($scope, FactoryDog) {
-  	$scope.dogs = null;
-    
-    var init = function(){
-        FactoryDog.getAll().success(function(data, status){
-            if(status == 200){
-                $scope.dogs = data;
-            }
-            console.log($scope.dogs);
-        });
-    };
-    
-    init();
-};
+    .controller('MainCtrl', function ($scope, FactoryDog) {
+        $scope.dogs = null;
+
+        var init = function () {
+            FactoryDog.getAll().success(function (data, status) {
+                if (status === 200) {
+                    $scope.dogs = data;
+                }
+                console.log($scope.dogs);
+            });
+        };
+
+        init();
+    });
