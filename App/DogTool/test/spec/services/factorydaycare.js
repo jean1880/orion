5fd93522e191706a134218c2b1d966f24200346b
AmusableBman <AmusableBman@gmail.com>
2015-03-11 16:38:20 -0400

    DaycareFactory testing file

--- App/DogTool/test/spec/services/factorydaycare.js
+++ App/DogTool/test/spec/services/factorydaycare.js
@@ -7,12 +7,70 @@ describe('Service: factorydaycare', function () {
 
   // instantiate service
   var factorydaycare;
-  beforeEach(inject(function (_factorydaycare_) {
-    factorydaycare = _factorydaycare_;
+      $http;
+	  SailsRoute;
+  
+  beforeEach(inject(function($injector) {
+    factorydaycare = $injector.get('factorydaycare');
+	$http = injector.get('$http');
+	SailsRoute = $injector.get('SailsRoute');
   }));
-
-  it('should do something', function () {
-    expect(!!factorydaycare).toBe(true);
+  
+	
+  describe('get()', function () {
+	var daycareID;
+	var response;
+	var callValue
+	beforeEach(function () {
+	  callValue = {msg: 'hi' };
+	  spyOn($http, 'get').and.returnValue(callValue);
+	  
+	  daycareID = chance.natural();
+	  
+	  response = factorydaycare.get(daycareID);
+	});
+	
+    it('will make a get call to sails', function () {
+      expect($http.get).toHaveBeenCalled();
+    });
+	
+	it('will get the correct daycare from sails', function () {
+		expect($http.get).toHaveBeenCalledWith(SailsRoute.daycare.get(daycareID));
+	});
+	
+	it('will return the response from sails', function () {
+      expect(response).toBe(callValue);
+    });
   });
-
+  
+  describe('get()', function () {
+	var daycare;
+	var returned;
+	
+	beforeEach(function () {
+	  daycare = Mockery.mockDaycare();
+	  
+	  spyOn(%http, 'post').and.returnValue(daycare);
+	
+	  returned = factorydaycare.post(daycare);
+	});
+	
+    it('will make a post call to sails', function () {
+      expect($http.post).toHaveBeenCalled();
+    });
+	
+	it('will pass a daycare to sails', function () {
+      expect($http.post).toHaveBeenCalledWith(jasmine.any(String), daycare);
+    });
+	
+	it('will pass a daycare to sails', function () {
+      expect($http.post).toHaveBeenCalledWith(SailsRoute.Daycare.route, jasmine.any(Object));
+    });
+	
+	it('will return a response form sails', function () {
+      expect(returned).toBe(daycare);
+    });
+	
+  });
+  
 });
