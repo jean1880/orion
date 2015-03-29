ec5b43c5f04cc276c616a3e305ce78e6c6db9b2a
Stephen A. Wilson <Stephen-356@hotmail.com>
2015-03-29 15:02:23 -0400

    move tests to the new controller

--- App/DogTool/test/spec/controllers/dog-view.js
+++ App/DogTool/test/spec/controllers/dog-view.js
@@ -1,7 +1,6 @@
 'use strict';
 
 /* global Mockery */
-/* global chance */
 
 describe('Controller: DogViewCtrl', function () {
 
@@ -15,8 +14,7 @@ describe('Controller: DogViewCtrl', function () {
     SailsRoute,
     $location,
     flash,
-    dogGetHandler,
-    dogPostHandler;
+    dogGetHandler;
 
   var dog;
 
@@ -42,8 +40,6 @@ describe('Controller: DogViewCtrl', function () {
     };
 
     dogGetHandler  = $httpBackend.whenGET(SailsRoute.Dog.get(dog.id)).respond(200, dog);
-    dogPostHandler = $httpBackend.whenPOST(SailsRoute.Dog.get(dog.id)).respond(200, dog);
-
   }));
 
   var runController = inject(function ($controller) {
@@ -91,133 +87,6 @@ describe('Controller: DogViewCtrl', function () {
 
     it('redirects to homepage', function () {
       expect($location.path).toHaveBeenCalledWith('/');
-    });
-  });
-
-  describe('$scope.editInfoBtn()', function () {
-    beforeEach(function () {
-      runController();
-      scope.editInfoBtn();
-    });
-
-    it('sets $scope.editingInfo to true', function () {
-      expect(scope.editingInfo).toBeTruthy();
-    });
-
-    it('makes a duplicate of $scope.dog', function () {
-      expect(scope.editedDog).not.toBe(scope.dog);
-    });
-  });
-
-  describe('$scope.saveInfoBtn()', function () {
-    beforeEach(function () {
-      runController();
-    });
-
-    describe('when not editing info', function () {
-      beforeEach(function () {
-        scope.editingInfo = false;
-      });
-
-      describe('when called', function () {
-        beforeEach(function () {
-          scope.saveInfoBtn();
-        });
-
-        it('does not attempt to update the dog', function () {
-          expect(FactoryDog.update).not.toHaveBeenCalled();
-        });
-      });
-    });
-
-    describe('when editing info', function () {
-      beforeEach(function () {
-        scope.editInfoBtn();
-      });
-
-      describe('when form is not dirty', function () {
-        beforeEach(function () {
-          scope.infoForm.$dirty = false;
-        });
-
-        describe('when called', function () {
-          beforeEach(function () {
-            scope.saveInfoBtn();
-          });
-
-          it('does not attempt to update the dog', function () {
-            expect(FactoryDog.update).not.toHaveBeenCalled();
-          });
-        });
-      });
-
-      describe('when form is dirty', function () {
-        beforeEach(function () {
-          scope.infoForm.$dirty = true;
-          scope.editedDog.Breed = chance.word({length: 2});
-        });
-
-        describe('when called', function () {
-          beforeEach(function () {
-            scope.saveInfoBtn();
-            $httpBackend.flush();
-          });
-
-          it('attempts to update the edited dog', function () {
-            expect(FactoryDog.update).toHaveBeenCalledWith(scope.editedDog);
-          });
-
-          it('does not attempt to update the original dog', function () {
-            expect(FactoryDog.update).not.toHaveBeenCalledWith(scope.dog);
-          });
-        });
-
-        describe('when updating is successful', function() {
-          beforeEach(function () {
-            scope.saveInfoBtn();
-            $httpBackend.flush();
-          });
-
-          it('sets $scope.editingInfo to false', function () {
-            expect(scope.editingInfo).toBeFalsy();
-          });
-        });
-
-        describe('when updating fails', function() {
-          var response = {};
-
-          beforeEach(function () {
-            dogPostHandler.respond(400, response);
-            scope.saveInfoBtn();
-            $httpBackend.flush();
-          });
-
-          it('does not set $scope.editingInfo to false', function () {
-            expect(scope.editingInfo).not.toBeFalsy();
-          });
-        });
-      });
-    });
-  });
-
-  describe('$scope.cancelInfoBtn()', function () {
-    beforeEach(function () {
-      runController();
-    });
-
-    describe('when called', function () {
-      beforeEach(function () {
-        scope.editingInfo = true;
-        scope.cancelInfoBtn();
-      });
-
-      it('does not attempt to update the dog', function () {
-        expect(FactoryDog.update).not.toHaveBeenCalled();
-      });
-
-      it('sets $scope.editingInfo to false', function () {
-        expect(scope.editingInfo).not.toBeTruthy();
-      });
     });
   });
 });
