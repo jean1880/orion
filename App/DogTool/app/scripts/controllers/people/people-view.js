3cd8d4dfccd2b4c68182ea47d8d65dbb9f6c8b65
Stephen A. Wilson <Stephen-356@hotmail.com>
2015-03-30 16:24:27 -0400

    build view controller

--- App/DogTool/app/scripts/controllers/people/people-view.js
+++ App/DogTool/app/scripts/controllers/people/people-view.js
@@ -8,10 +8,35 @@
  * Controller of the dogToolApp
  */
 angular.module('dogToolApp')
-  .controller('PeopleViewCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('PeopleViewCtrl', function ($scope, FactoryPeople, $location, $routeParams) {
+    /**
+     * Initalizes the controller for use
+     *
+     * @private
+     * @method init
+     */
+    var init = function() {
+      loadPerson($routeParams.id);
+    };
+
+    /**
+     * Loads a person from sails into the scope
+     *
+     * Redirects the user to the home page if the person does not exist
+     *
+     * @private
+     * @method loadPerson
+     * @param {ID} id The id of the person to load
+     */
+    var loadPerson = function (id) {
+      FactoryPeople.get(id)
+        .success(function (response) {
+          $scope.person = response;
+        })
+        .error(function () {
+          $location.path('/');
+        });
+    };
+
+    init();
   });
