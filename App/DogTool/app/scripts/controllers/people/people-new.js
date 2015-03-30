28d04c9478203a61da1b69a2eabff2c48ba3d0a4
Stephen A. Wilson <Stephen-356@hotmail.com>
2015-03-30 15:56:32 -0400

    discovered nested models are saved implicitly

--- App/DogTool/app/scripts/controllers/people/people-new.js
+++ App/DogTool/app/scripts/controllers/people/people-new.js
@@ -8,10 +8,48 @@
  * Controller of the dogToolApp
  */
 angular.module('dogToolApp')
-  .controller('PeopleNewCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('PeopleNewCtrl', function ($scope, FactoryPeople, FactoryAddress, flash, $location) {
+    /**
+     * Initalizes the controller for use
+     *
+     * @private
+     * @method init
+     */
+    var init = function () {
+      $scope.person = {
+        Address: {}
+      };
+    };
+
+    /**
+     * save button handler for the form
+     *
+     * If the form is valid, saves the new dog to the database, and redirects
+     * to the view page for the new dog.
+     *
+     * @method saveBtn
+     **/
+    $scope.submitForm = function() {
+      if($scope.newPersonForm.$valid) {
+        savePerson();
+      }
+    };
+
+    var savePerson = function () {
+      FactoryPeople.post($scope.person)
+        .success(processPersonSuccess)
+        .error(processPersonError);
+    };
+
+    var processPersonSuccess = function (response) {
+      flash.success = 'Person Created';
+      $location.path('/person/' + response.id);
+    };
+
+    var processPersonError = function (response) {
+      flash.error = 'Error saving person';
+      console.log('Error saving person: ' + JSON.stringify(response));
+    };
+
+    init();
   });
