bf0fdde236ac73859ba8d1495a742e30b7559029
Stephen A. Wilson <Stephen-356@hotmail.com>
2015-03-30 16:02:50 -0400

    add doc blocks

--- App/DogTool/app/scripts/controllers/people/people-new.js
+++ App/DogTool/app/scripts/controllers/people/people-new.js
@@ -31,22 +31,45 @@ angular.module('dogToolApp')
      **/
     $scope.submitForm = function() {
       if($scope.newPersonForm.$valid) {
-        savePerson();
+        savePerson($scope.person);
       }
     };
 
-    var savePerson = function () {
-      FactoryPeople.post($scope.person)
-        .success(processPersonSuccess)
-        .error(processPersonError);
+    /**
+     * saves the given person to the sails database
+     *
+     * @private
+     * @method savePerson
+     * @param {Person} person The person to save
+     **/
+    var savePerson = function (person) {
+      FactoryPeople.post(person)
+        .success(processSuccess)
+        .error(processError);
     };
 
-    var processPersonSuccess = function (response) {
+    /**
+     * handles a successful response from sails after saving the person
+     *
+     * Redirects the user to the view page for the newly created person
+     *
+     * @private
+     * @method processSuccess
+     * @param {Sails.response} response The successful response from sails
+     **/
+    var processSuccess = function (response) {
       flash.success = 'Person Created';
       $location.path('/person/' + response.id);
     };
 
-    var processPersonError = function (response) {
+    /**
+     * handles a failed response from sails after saving the person
+     *
+     * @private
+     * @method processError
+     * @param {Sails.response} response The error response from sails
+     **/
+    var processError = function (response) {
       flash.error = 'Error saving person';
       console.log('Error saving person: ' + JSON.stringify(response));
     };
