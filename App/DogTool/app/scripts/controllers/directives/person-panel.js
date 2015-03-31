1f3306e04012ec58f4cb19a03cc917ba5e6cf270
Stephen A. Wilson <Stephen-356@hotmail.com>
2015-03-30 23:23:12 -0400

    add documentation for panel controller

--- App/DogTool/app/scripts/controllers/directives/person-panel.js
+++ App/DogTool/app/scripts/controllers/directives/person-panel.js
@@ -9,6 +9,12 @@
  */
 angular.module('dogToolApp')
   .controller('PersonPanelCtrl', function ($scope, FactoryAddress) {
+    /**
+     * Initalizes the controller for use
+     *
+     * @private
+     * @method init
+     */
     var init = function () {
       $scope.$watch('person', function(person) {
         if(person) {
@@ -17,13 +23,30 @@ angular.module('dogToolApp')
       });
     };
 
-    var loadAddressForPerson = function (person) {
+    /**
+     * loads the address for the given person if it has not been loaded yet
+     *
+     * @private
+     * @method loadAddressForPerson
+     * @param {Person} person The person to load the address for.
+     * @param {function()} callback Function to call when the address is loaded
+     */
+    var loadAddressForPerson = function (person, callback) {
       if(typeof person.Address === 'string') {
         FactoryAddress.get(person.Address)
           .success(function (response) {
             person.Address = response;
           })
-          .error(function () {});
+          .then(function () {
+            if (callback) {
+              callback();
+            }
+          });
+      }
+      else {
+        if (callback) {
+          callback();
+        }
       }
     };
 
