8213bf3c0bc02544aa833d01de7c54b012c85bba
Stephen A. Wilson <Stephen-356@hotmail.com>
2015-04-04 16:44:50 -0400

    document dog-view controller

--- App/DogTool/app/scripts/controllers/dog-view.js
+++ App/DogTool/app/scripts/controllers/dog-view.js
@@ -7,16 +7,32 @@
  * @name dogToolApp.controller:DogViewCtrl
  * @description
  * # DogCtrl
- * Controller of the dogToolApp
+ *
+ * Controller for the viewing of a dog based on a id from routeParams
  */
 angular.module('dogToolApp')
   .controller('DogViewCtrl', function ($scope, $routeParams, $location, FactoryDog) {
+    /**
+     * Prepares the controller for use
+     *
+     * @private
+     * @method init
+     */
     var init = function() {
       loadDog($routeParams.id);
 
       $scope.editingInfo = false;
     };
 
+    /**
+     * Loads a given dog from sails
+     *
+     * If the dog is not found, redirects to the homepage
+     *
+     * @private
+     * @method loadDog
+     * @param  {ID} id The id of the dog to load from sails
+     */
     var loadDog = function (id) {
       FactoryDog.get(id)
         .success(function (response) {
@@ -31,6 +47,16 @@ angular.module('dogToolApp')
         });
     };
 
+    /**
+     * Converts the raw data on the given dog into more manageable objects
+     *
+     * - birthdate is converted to a Date Object
+     * - age is populated from the birthdate
+     *
+     * @private
+     * @method processDog
+     * @param  {Dog} dog The dog to update
+     */
     var processDog = function (dog) {
       dog.Birthdate = new Date(dog.Birthdate);
       dog.Age = moment(dog.Birthdate).fromNow(true);
