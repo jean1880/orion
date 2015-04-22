95d8d7440983321189b1e3d561bc1431163e4752
Stephen A. Wilson <Stephen-356@hotmail.com>
2015-04-22 15:00:49 -0400

    Add tests for personPanel link function

--- App/DogTool/test/spec/directives/person-panel.js
+++ App/DogTool/test/spec/directives/person-panel.js
@@ -5,16 +5,65 @@ describe('Directive: personPanel', function () {
   // load the directive's module
   beforeEach(module('dogToolApp'));
 
-  var element,
+  //load html files
+  beforeEach(module('htmlFiles'));
+
+  var
+    $compile,
+    $rootScope;
+
+  var
+    element,
     scope;
 
-  beforeEach(inject(function ($rootScope) {
+  var buildElement = function (html) {
+    html = angular.element(html);
+    return $compile(html)(scope);
+  };
+
+  //stub out nested directives
+  beforeEach(module('dogToolApp', function($provide){
+    $provide.factory('personInfoDirective',         function(){ return {}; });
+    $provide.factory('personSelectCreateDirective', function(){ return {}; });
+  }));
+
+  beforeEach(inject(function ($injector) {
+    $compile = $injector.get('$compile');
+    $rootScope = $injector.get('$rootScope');
+
     scope = $rootScope.$new();
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<dog-person-panel></dog-person-panel>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the personPanel directive');
-  }));
+  describe('when unlinkable is undefined', function () {
+    beforeEach(function () {
+      element = buildElement('<person-panel></person-panel>');
+      $rootScope.$digest();
+    });
+
+    it('sets $scope.unlinkable to true', function () {
+      expect(element.isolateScope().unlinkable).toBe(true);
+    });
+  });
+
+  describe('when unlinkable is true', function () {
+    beforeEach(function () {
+      element = buildElement('<person-panel unlinkable="true"></person-panel>');
+      $rootScope.$digest();
+    });
+
+    it('sets $scope.unlinkable to true', function () {
+      expect(element.isolateScope().unlinkable).toBe(true);
+    });
+  });
+
+  describe('when unlinkable is false', function () {
+    beforeEach(function () {
+      element = buildElement('<person-panel unlinkable="false"></person-panel>');
+      $rootScope.$digest();
+    });
+
+    it('sets $scope.unlinkable to false', function () {
+      expect(element.isolateScope().unlinkable).toBe(false);
+    });
+  });
 });
