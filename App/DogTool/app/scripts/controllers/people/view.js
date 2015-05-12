872aec3c8c896b11e000aa66e56f99509f108a0e
Stephen A. Wilson <Stephen-356@hotmail.com>
2015-05-11 20:07:32 -0400

    add edit area to person info

--- App/DogTool/app/scripts/controllers/people/view.js
+++ App/DogTool/app/scripts/controllers/people/view.js
@@ -34,5 +34,47 @@ angular.module('dogToolApp')
         });
     };
 
+    /**
+     * handler for the edit button
+     *
+     * Switches the panel into editing mode when called.
+     *
+     * @method editInfoBtn
+     */
+    $scope.editInfoBtn = function() {
+      $scope.editedPerson = angular.copy($scope.person);
+      $scope.editingInfo = true;
+    };
+
+    /**
+     * handler for the cancel button
+     *
+     * Reverts all changes to the dog, and return the panel to viewing mode
+     *
+     * @method cancelInfoBtn
+     */
+    $scope.cancelInfoBtn = function() {
+      $scope.editingInfo = false;
+    };
+
+    /**
+     * handler for the save button
+     *
+     * if the form is valid, saves changes to the database, and return the panel
+     * to viewing mode
+     *
+     * @method saveInfoBtn
+     */
+    $scope.editInfoSubmit = function() {
+      FactoryPeople.update($scope.editedPerson)
+        .success(function (response) {
+          $scope.person = response;
+          $scope.editingInfo = false;
+        })
+        .error(function () {
+          flash.error = 'An error occured';
+        });
+    };
+
     init();
   });
