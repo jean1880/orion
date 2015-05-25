9e04b395de834d459a4e87b0387268db89839af5
Stephen A. Wilson <Stephen-356@hotmail.com>
2015-05-25 10:04:37 -0400

    add tests for peopleListCtrl

--- App/DogTool/test/spec/controllers/people/people-list.js
+++ App/DogTool/test/spec/controllers/people/people-list.js
@@ -1,22 +1,77 @@
 'use strict';
 
+/* global Mockery */
+
 describe('Controller: PeopleListCtrl', function () {
 
   // load the controller's module
   beforeEach(module('dogToolApp'));
 
   var PeopleListCtrl,
-    scope;
+    scope,
+    FactoryPeople,
+    $httpBackend,
+    SailsRoute,
+    flash;
+
+  var peopleGetHandler;
+
+  var people;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
+  beforeEach(inject(function ($rootScope, $injector) {
+    scope         = $rootScope.$new();
+    FactoryPeople = $injector.get('FactoryPeople');
+    $httpBackend  = $injector.get('$httpBackend');
+    SailsRoute    = $injector.get('SailsRoute');
+    flash         = $injector.get('flash');
+
+    people = [Mockery.mockPerson()];
+
+    peopleGetHandler = $httpBackend.whenGET(SailsRoute.People.getAll).respond(200, people);
+
+    spyOn(FactoryPeople, 'getAll').and.callThrough();
+  }));
+
+  var runController = inject(function($controller) {
     PeopleListCtrl = $controller('PeopleListCtrl', {
       $scope: scope
     });
-  }));
+  });
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  describe('when loaded', function() {
+    it('tries to load all people', function () {
+      runController();
+
+      expect(FactoryPeople.getAll).toHaveBeenCalled();
+    });
+
+    describe('when loading all people is successful', function() {
+      beforeEach(function() {
+        peopleGetHandler.respond(200, people);
+
+        runController();
+
+        $httpBackend.flush();
+      });
+
+      it('stores the people in $scope.people', function () {
+        expect(scope.people).toEqual(people);
+      });
+    });
+
+    describe('when loading all people is unsuccessful', function() {
+      beforeEach(function() {
+        peopleGetHandler.respond(500, {message: 'internal error'});
+
+        runController();
+
+        $httpBackend.flush();
+      });
+
+      it('sets an error flash', function () {
+        expect(flash.error).toBeDefined();
+      });
+    });
   });
 });
