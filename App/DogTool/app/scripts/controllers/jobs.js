6f0356bca67e71e0d968be8b82f09b2aaeca491e
Kevin <intrepidhonor@gmail.com>
2015-06-08 12:53:15 -0400

    merge changes with develop branch

--- App/DogTool/app/scripts/controllers/jobs.js
+++ App/DogTool/app/scripts/controllers/jobs.js
@@ -9,11 +9,6 @@
  */
 angular.module('dogToolApp')
   .controller('JobsCtrl', function ($scope,$routeParams, $location, FactoryDog, FactoryNote) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
     //Add test connection to the page when loaded
     $scope.Job= {titleOf:"Job Title Place Holder",hasJobId:true};
     $scope.JobList= [{
