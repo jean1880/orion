4f150e05e1ba6b359c5cbcf4445905784b94dd62
Jean-Luc Desroches <jean18801880@gmail.com>
2015-06-12 07:43:30 -0400

    fixes

--- App/DogTool/app/scripts/controllers/jobs.js
+++ App/DogTool/app/scripts/controllers/jobs.js
@@ -27,19 +27,23 @@ angular.module('dogToolApp')
 
   $scope.JobList = [{
       "id": 435436,
-      "title": "test job",
+      "Name": "test job",
       "dateStart": "Apr.16/15 : 10:00am",
       "dateEnd": "Apr.16/15 : 1:00pm",
-      "location": "123 Fake Street",
+      "Location": {
+        "Street": "123 Fake Street"
+      },
       "type": "Daycare",
       "img": ["http://loremflickr.com/300/300/dog?random=154", "http://loremflickr.com/300/300/dog?random=1660"]
 },
     {
       "id": 43546,
-      "title": "test job2",
+      "Name": "test job2",
       "dateStart": "Jan 14, 2015 1:00pm",
       "dateEnd": "Jan 15,2015 6:00pm",
-      "location": "123 Fake Street",
+      "Location": {
+        "Street": "123 Fake Street"
+      },
       "type": "In-Home",
       "img": ["http://loremflickr.com/300/300/dog?random=14", "http://loremflickr.com/300/300/dog?random=160", "http://loremflickr.com/300/300/dog?random=60"]
 }
