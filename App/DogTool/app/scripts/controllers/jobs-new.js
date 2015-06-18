94a28fbf1dabd9a9506aa9dd6d4cf22f18b55ce0
Kevin <intrepidhonor@gmail.com>
2015-06-18 18:53:35 -0400

    add controllers for the new job

--- App/DogTool/app/scripts/controllers/jobs-new.js
+++ App/DogTool/app/scripts/controllers/jobs-new.js
@@ -3,16 +3,24 @@
 /**
  * @ngdoc function
  * @name dogToolApp.controller:NewjobsCtrl
- * @description
+ * @description 
  * # NewjobsCtrl
  * Controller of the dogToolApp
  */
 angular.module('dogToolApp')
   .controller('NewJobsCtrl', function ($scope, $location, FactoryDog, flash) {
+  $scope.bookingDogs=[];
   var init = function() {
       loadAllDogs();
     };
-
+    /**
+     * @method loadAllDogs
+     * @return loads the dogs into a list var of scope 
+     * dogs
+     * @description links to dog factory "FactoryDog" to 
+     * get the dog list
+     * 
+     */
     var loadAllDogs = function () {
       $scope.dogs = null;
 
@@ -24,10 +32,31 @@ angular.module('dogToolApp')
           flash.error = 'A error occured while loading dogs.';
         });
     };
-
+    /**
+     * @method loadAllDogs
+     * @description creates the new booking
+     * @returns an object with two keys. responseCode and msg.
+     * @returns responseCode, if 200 sucess else different errors.
+     * @returns msg, is a generica user message.
+     */
     init();
-    var createJob = function()
+     $scope.createBooking = function()
     {
       
     }
+    
+    /**
+     * @method loadAllDogs
+     * @return loads the dogs into a list var of scope 
+     * dogs
+     * @description links to dog factory "FactoryDog" to 
+     * get the dog list
+     * 
+     */
+    $scope.bookDog = function(dog)
+    {
+      console.log("summoned");
+      $scope.bookingDogs.push(dog);
+    }
+    
   });
