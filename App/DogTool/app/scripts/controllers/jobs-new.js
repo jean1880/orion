086e9942e14aaccb71be2a83bfd051ce24c9d46c
Kevin <intrepidhonor@gmail.com>
2015-06-23 13:33:30 -0400

    fix some functionality issues

--- App/DogTool/app/scripts/controllers/jobs-new.js
+++ App/DogTool/app/scripts/controllers/jobs-new.js
@@ -3,51 +3,68 @@
 /**
  * @ngdoc function
  * @name dogToolApp.controller:NewjobsCtrl
- * @description 
+ * @description
  * # NewjobsCtrl
  * Controller of the dogToolApp
  */
 angular.module('dogToolApp')
   .controller('NewJobsCtrl', function ($scope, $location, FactoryDog, flash, FactoryJob, FactoryJobType) {
-  
-  var now =new Date();
-  $scope.booking={Name:'',Dogs:[],Costs:[],Notes:[],Invoice:[],Jobtype:{Name:'Day Care',Description:'test'},Location:[],Calendars:{StartDate: now, EndDate: now}};  
-  
-  var init = function() {
-      loadAllDogs();
-      FactoryJobType.getAll()
-      .success(function (response) {
-        console.log(response);
-        $scope.jobTypes=response;
-         flash.success = 'job types loaded.';
-      })
-      .error(function () {
-        flash.error = 'An error occured while loading job types.';
-      });
+
+    $scope.pageType = "New ";
+    $scope.selectedJobType;
+    $scope.addedDogUI = [];
+    var now = new Date();
+    $scope.booking = {
+      Name: '',
+      Dogs: [],
+      Costs: [],
+      Notes: [],
+      Invoice: [],
+      Jobtype: {
+        id: null
+      },
+      Location: {},
+      Calendars: {
+        StartDate: now,
+        EndDate: now
+      }
     };
 
-  // setup the datepicker directives
-  
-  $scope.dateOptions = {
-    startingDay: 1,
-    showWeeks: false
-  };
-  
-  $scope.hourStep = 1;
-  $scope.minuteStep = 1;
+    var init = function () {
+      loadAllDogs();
+      FactoryJobType.getAll()
+        .success(function (response) {
+          console.log(response);
+          $scope.jobTypes = response;
+          flash.success = 'job types loaded.';
+        })
+        .error(function () {
+          flash.error = 'An error occured while loading job types.';
+        });
+    };
 
-  $scope.timeOptions = {
-    hourStep: [1, 2, 3],
-    minuteStep: [1, 5, 10, 15, 25, 30]
-  };
-  
+    // setup the datepicker directives
+
+    $scope.dateOptions = {
+      startingDay: 1,
+      showWeeks: false
+    };
+
+    $scope.hourStep = 1;
+    $scope.minuteStep = 1;
+
+    $scope.timeOptions = {
+      hourStep: [1, 2, 3],
+      minuteStep: [1, 5, 10, 15, 25, 30]
+    };
+
     /**
      * @method loadAllDogs
-     * @return loads the dogs into a list var of scope 
+     * @return loads the dogs into a list var of scope
      * dogs
-     * @description links to dog factory "FactoryDog" to 
+     * @description links to dog factory "FactoryDog" to
      * get the dog list
-     * 
+     *
      */
     var loadAllDogs = function () {
       $scope.dogs = null;
@@ -60,73 +77,77 @@ angular.module('dogToolApp')
           flash.error = 'An error occured while loading dogs.';
         });
     };
-    
+
     init();
-  
- 
+
+
     /**
      * @method createBooking
      * @description creates the new booking through FactoryJob via post
      */
-     $scope.createBooking = function()
-    {
-      FactoryJob.post($scope.booking).success(function(res){
-        flash.success = 'Job Created.';
-        
-        console.log(res);
-      })
-      .error(function(err){
-        console.log(err);
-        flash.error='An error occured while creating a new Job. Sorry but this job was not created.';
-      });
+    $scope.createBooking = function () {
+      $scope.booking.Jobtype.id = $scope.selectedJobType.id;
+      FactoryJob.post($scope.booking).success(function (res) {
+          flash.success = 'Job Created.';
+console.log("booking Data");
+        console.log($scope.booking);
+        console.log("response data");
+          console.log(res);
+        })
+        .error(function (err) {
+          console.log(err);
+          flash.error = 'An error occured while creating a new Job. Sorry but this job was not created.';
+        });
     }
-    
+
     /**
      * @method bookDog
      * @description Adds dogs to the booking list and removes the dogs from the search list (dogs avaliable to be added)
-     * 
+     *
      */
-    $scope.bookDog = function(indexIn)
-    {
-      console.log("Log booking");
-      console.log($scope.booking);
-      var dogIn = $scope.dogs[indexIn];
-      console.log(dogIn);
-      $scope.dogs.splice(indexIn,1);
-      $scope.booking.Dogs.push(dogIn);
-    }
-    /**
-     * @method removeDog
-     * @description Removes dogs from the booking and pushes them back to the search list.
-     * 
-     */
-    $scope.removeDog = function(indexOut)
-    {
-      var dogOut = $scope.booking.Dogs[indexOut];
-      console.log(dogOut);
-      $scope.booking.Dogs.splice(indexOut,1);
-      $scope.dogs.push(dogOut);
-    }
-    /**
-     * @method addFee
-     * @description Adds a list of fees to the job
-     * 
-     */
-    $scope.addFee=function()
-    {
-      if($scope.feeDescription!='' && $scope.feeAmount!= null){
-        var newCost={Date:new Date(),Description:$scope.feeDescription,Cost:$scope.feeAmount};
+    $scope.bookDog = function (indexIn) {
+        console.log("Log booking");
+        console.log($scope.booking);
+        var dogIn = $scope.dogs[indexIn];
+        console.log(dogIn);
+        $scope.dogs.splice(indexIn, 1);
+        $scope.addedDogUI.push(dogIn);
+        $scope.booking.Dogs.push(dogIn.id);
+      }
+      /**
+       * @method removeDog
+       * @description Removes dogs from the booking and pushes them back to the search list.
+       *
+       */
+    $scope.removeDog = function (indexOut) {
+        console.log("Log booking remove");
+        var dogOut = $scope.addedDogUI[indexOut];
+        console.log(dogOut);
+        $scope.dogs.push(dogOut);
+        $scope.booking.Dogs.splice(indexOut, 1);
+        $scope.addedDogUI.splice(indexOut, 1);
+
+      }
+      /**
+       * @method addFee
+       * @description Adds a list of fees to the job
+       *
+       */
+    $scope.addFee = function () {
+      if ($scope.feeDescription != '' && $scope.feeAmount != null) {
+        var newCost = {
+          Date: new Date(),
+          Description: $scope.feeDescription,
+          Cost: $scope.feeAmount
+        };
         console.log(newCost);
         $scope.booking.Costs.push(newCost);
-      }
-      else
-      {
-        if($scope.feeDescription==''){
-          flash.error='Please enter a description';
+      } else {
+        if ($scope.feeDescription == '') {
+          flash.error = 'Please enter a description';
         }
-        if($scope.feeAmount==null)
-        {
-          flash.error='Please enter a fee amount in proper currency';
+        if ($scope.feeAmount == null) {
+          flash.error = 'Please enter a fee amount in proper currency';
         }
       }
     }
--- App/DogTool/app/scripts/controllers/jobs.js
+++ App/DogTool/app/scripts/controllers/jobs.js
@@ -9,23 +9,86 @@
  */
 angular.module('dogToolApp')
 
-.controller('JobsCtrl', function ($scope, $location, FactoryDog, flash, FactoryJob) {
-  
-  var now =new Date();
-  $scope.booking={Name:'',Dogs:[],Costs:[],Notes:[],Invoice:[],JobType:{Name:'Day Care',Description:'test'},Location:[],Calendars:{StartDate: now, EndDate: now}};  
-  
-  var init = function() {
-      loadAllDogs();
-      
-    };
+.controller('JobsCtrl', function ($scope, $location, FactoryDog, flash, FactoryJob, FactoryJobType, $routeParams) {
+
+  $scope.pageType = "Edit ";
+  $scope.selectedJobType;
+  $scope.addedDogUI = [];
+
+  var now = new Date();
+  $scope.booking = {
+    Name: '',
+    Dogs: [],
+    Costs: [],
+    Notes: [],
+    Invoice: [],
+    Jobtype: {
+      id: null
+    },
+    Location: [],
+    Calendars: {
+      StartDate: now,
+      EndDate: now
+    }
+  };
+
+  var getBookingTypes = function () {
+    //get the booking types
+    FactoryJobType.getAll()
+      .success(function (response) {
+        //        console.log(response);
+        $scope.jobTypes = response;
+        flash.success = 'job types loaded.';
+      })
+      .error(function () {
+        flash.error = 'An error occured while loading job types.';
+      });
+  }
+  var loadBookingData = function () {
+    //get the specific booking information
+    FactoryJob.get($routeParams.id)
+      .success(function (res) {
+        console.log("Booking Data");
+        console.log(res);
+        $scope.booking = res;
+        
+        $scope.addedDogUI = res.Dogs;
+        
+        $scope.selectedJobType = res.Jobtype;
+        console.log("Dog ID setup");
+        console.log($scope.addedDogUI);
+        $scope.booking.Jobtype = $scope.booking.Jobtype.id;
+//        for (var dogId in $scope.addedDogUI) {
+//          console.log("Dog ID test");
+//          var dogFind = $scope.addedDogUI[dogId];
+//          console.log(dogFind);
+//          $scope.booking.Dogs.push(dogFind.id);
+//          console.log($scope.booking.Dogs);
+//          var indexRem = $scope.dogs.indexOf(dogFind);
+//          console.log("Index test");
+//          console.log($scope.dogs);
+//          console.log(indexRem);
+////          $scope.dogs.splice(indexRem,1);
+//        }
+      })
+      .error(function () {
+        flash.error = 'An error occured. Unable to load booking information';
+      });
+
+  }
+  var init = function () {
+    loadAllDogs();
+    getBookingTypes();
+    loadBookingData();
+  };
 
   // setup the datepicker directives
-  
+
   $scope.dateOptions = {
     startingDay: 1,
     showWeeks: false
   };
-  
+
   $scope.hourStep = 1;
   $scope.minuteStep = 1;
 
@@ -33,93 +96,95 @@ angular.module('dogToolApp')
     hourStep: [1, 2, 3],
     minuteStep: [1, 5, 10, 15, 25, 30]
   };
-  
-    /**
-     * @method loadAllDogs
-     * @return loads the dogs into a list var of scope 
-     * dogs
-     * @description links to dog factory "FactoryDog" to 
-     * get the dog list
-     * 
-     */
-    var loadAllDogs = function () {
-      $scope.dogs = null;
 
-      FactoryDog.getAll()
-        .success(function (response) {
-          $scope.dogs = response;
-        })
-        .error(function () {
-          flash.error = 'An error occured while loading dogs.';
-        });
-    };
-    
-    init();
-  
- 
-    /**
-     * @method createBooking
-     * @description creates the new booking through FactoryJob via post
-     */
-     $scope.createBooking = function()
-    {
-      FactoryJob.post($scope.booking).success(function(res){
+  /**
+   * @method loadAllDogs
+   * @return loads the dogs into a list var of scope
+   * dogs
+   * @description links to dog factory "FactoryDog" to
+   * get the dog list
+   *
+   */
+  var loadAllDogs = function () {
+    $scope.dogs = null;
+
+    FactoryDog.getAll()
+      .success(function (response) {
+        $scope.dogs = response;
+      })
+      .error(function () {
+        flash.error = 'An error occured while loading dogs.';
+      });
+  };
+
+  init();
+
+
+  /**
+   * @method createBooking
+   * @description creates the new booking through FactoryJob via post
+   */
+  $scope.createBooking = function () {
+    $scope.booking.Jobtype.id = $scope.selectedJobType.id;
+    console.log("Booking Obj:");
+    console.log($scope.booking);
+    FactoryJob.update($scope.booking).success(function (res) {
         flash.success = 'Job Created.';
-        
+
         console.log(res);
       })
-      .error(function(err){
+      .error(function (err) {
         console.log(err);
-        flash.error='An error occured while creating a new Job. Sorry but this job was not created.';
+        flash.error = 'An error occured while creating a new Job. Sorry but this job was not created.';
       });
-    }
-    
-    /**
-     * @method bookDog
-     * @description Adds dogs to the booking list and removes the dogs from the search list (dogs avaliable to be added)
-     * 
-     */
-    $scope.bookDog = function(indexIn)
-    {
-      console.log($scope.booking);
+  }
+
+  /**
+   * @method bookDog
+   * @description Adds dogs to the booking list and removes the dogs from the search list (dogs avaliable to be added)
+   *
+   */
+  $scope.bookDog = function (indexIn) {
+      console.log("Log booking add");
       var dogIn = $scope.dogs[indexIn];
       console.log(dogIn);
-      $scope.dogs.splice(indexIn,1);
-      $scope.booking.Dogs.push(dogIn);
+      $scope.dogs.splice(indexIn, 1);
+      $scope.addedDogUI.push(dogIn);
     }
     /**
      * @method removeDog
      * @description Removes dogs from the booking and pushes them back to the search list.
-     * 
+     *
      */
-    $scope.removeDog = function(indexOut)
-    {
-      var dogOut = $scope.booking.Dogs[indexOut];
+  $scope.removeDog = function (indexOut) {
+      console.log("Log booking remove");
+      var dogOut = $scope.addedDogUI[indexOut];
       console.log(dogOut);
-      $scope.booking.Dogs.splice(indexOut,1);
       $scope.dogs.push(dogOut);
+      $scope.addedDogUI.splice(indexOut, 1);
+
     }
     /**
      * @method addFee
      * @description Adds a list of fees to the job
-     * 
+     *
      */
-    $scope.addFee=function()
-    {
-      if($scope.feeDescription!='' && $scope.feeAmount!= null){
-        var newCost={Date:new Date(),Description:$scope.feeDescription,Cost:$scope.feeAmount};
-        console.log(newCost);
-        $scope.booking.Costs.push(newCost);
+  $scope.addFee = function () {
+    if ($scope.feeDescription != '' && $scope.feeAmount != null) {
+      var newCost = {
+        Date: new Date(),
+        Description: $scope.feeDescription,
+        Cost: $scope.feeAmount
+      };
+      console.log(newCost);
+      $scope.booking.Costs.push(newCost);
+    } else {
+      if ($scope.feeDescription == '') {
+        flash.error = 'Please enter a description';
       }
-      else
-      {
-        if($scope.feeDescription==''){
-          flash.error='Please enter a description';
-        }
-        if($scope.feeAmount==null)
-        {
-          flash.error='Please enter a fee amount in proper currency';
-        }
+      if ($scope.feeAmount == null) {
+        flash.error = 'Please enter a fee amount in proper currency';
       }
     }
-  });
+  }
+});
