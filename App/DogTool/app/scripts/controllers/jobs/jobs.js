ea564a906d3d2de104bdb7eab6b18712d41d97fb
Jean-Luc Desroches <jean18801880@gmail.com>
2015-06-30 10:28:49 -0400

    now sending to job list page with correct dates

--- App/DogTool/app/scripts/controllers/jobs/jobs.js
+++ App/DogTool/app/scripts/controllers/jobs/jobs.js
@@ -8,7 +8,7 @@
  * Controller of the dogToolApp
  */
 angular.module('dogToolApp')
-  .controller('JobsCtrl', function ($scope, FactoryJob, $routeParams, factoryCalendar) {
+  .controller('JobsCtrl', function ($scope, FactoryDog, FactoryJob, $routeParams, factoryCalendar) {
 	$scope.JobList = [];
 
 	var LoadJobs = function(range) {
@@ -17,19 +17,40 @@ angular.module('dogToolApp')
   		}).success(function(data){
   			console.log(data);
   			$scope.JobList = data;
+  			for (var i = data.length - 1; i >= 0; i--) {
+  				for (var x = data[i].Dogs.length - 1; x >= 0; x--) {
+  					FactoryDog.get(data[i].Dogs[x].id).success(function(dog){
+  						for (var y = $scope.JobList.length - 1; y >= 0; y--) {
+  							for (var z = $scope.JobList[y].Dogs.length - 1; z >= 0; z--) {
+  								if($scope.JobList[y].Dogs[z].id == dog.id){
+  									$scope.JobList[y].Dogs[z] = dog;
+  								}
+  							};
+  						};
+  					});
+  				};
+  			};
   		});
 	};
 
 	var FetchDateEvents = function(){
+		var start = new Date($scope.date.valueOf());
+		start.setHours(start.getHours() - (start.getHours() - 24));
 		factoryCalendar.find({
 			StartDate: {
-				'<': $scope.date
+				'<=': start
 			},
-			EndDate: {
-				'>': $scope.date
-			}
+			or: [
+				{
+					IsAllDay: true
+				},
+				{
+					EndDate: {
+						'>=': $scope.date
+					}
+				}
+			]
 		}).success(function(data){
-			console.log(data);
 			$scope.events = data;
 			var ids = [];
 			for(var i = 0; i < $scope.events.length; i++){
