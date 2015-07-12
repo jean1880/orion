c287e0092cd093417aa694dc5319660ffcd9bbeb
Jean-Luc Desroches <jean18801880@gmail.com>
2015-07-12 13:29:52 -0400

    Invoice auto-created

--- Server/api/controllers/JobController.js
+++ Server/api/controllers/JobController.js
@@ -8,38 +8,34 @@ var async = require('async');
 module.exports = {
   create: function (req, res) {
     var self = req.body;
+    var charges = [];
 
-    console.log(self);
+    var deltaDate = 1
+    if (!self.Calendars.IsAllDay) {
+      deltaDate = new Date(new Date(self.Calendars.EndDate) - new Date(self.Calendars.StartDate));
+      deltaDate = deltaDate.getUTCDate() - 1;
+    }
+    deltaDate = deltaDate == 0 ? 1 : deltaDate;
 
+    // for each dog, create a charge, times the number of days
+    for (var i = self.Dogs.length; i >= 0; i--) {
+      charges.push({
+        Quantity: deltaDate,
+        JobType: self.Jobtype.id
+      });
+    }
 
+    // create an invoice from the booking
+    self.Invoice = {
+      Date: new Date(),
+      Charges: charges
+    };
+
+    // save invoice to system
     Job.create(self).exec(function (err, result) {
       if (!err) {
         console.log(result);
-
-        var charges = [];
-
-        var deltaDate = 1
-        if (!self.Calendars.IsAllDay) {
-          deltaDate = new Date(new Date(self.Calendars.EndDate) - new Date(self.Calendars.StartDate));
-          deltaDate = deltaDate.getUTCDate() - 1;
-        }
-
-        console.log("Date Delta: " + deltaDate);
-
-        // for each dog, create a charge, times the number of days
-        for (var i = self.Dogs.length; i >= 0; i--) {
-          charges.push({
-            Quantity: deltaDate
-
-          });
-        }
-
-        // create an invoice from the booking
-        /*Invoice.create({
-          Date: new Date(),
-          Job: result.id,
-          Charges:
-        })*/
+        res.send(result);
       }
     })
   }
