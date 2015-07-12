2c90d80dbea57d83aa375630751c0c7140ba9839
Jean-Luc Desroches <jean18801880@gmail.com>
2015-07-12 10:02:54 -0400

    save changes

--- Server/api/controllers/JobController.js
+++ Server/api/controllers/JobController.js
@@ -4,8 +4,43 @@
  * @description :: Server-side logic for managing jobs
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
-
+var async = require('async');
 module.exports = {
-	
-};
+  create: function (req, res) {
+    var self = req.body;
 
+    console.log(self);
+
+
+    Job.create(self).exec(function (err, result) {
+      if (!err) {
+        console.log(result);
+
+        var charges = [];
+
+        var deltaDate = 1
+        if (!self.Calendars.IsAllDay) {
+          deltaDate = new Date(new Date(self.Calendars.EndDate) - new Date(self.Calendars.StartDate));
+          deltaDate = deltaDate.getUTCDate() - 1;
+        }
+
+        console.log("Date Delta: " + deltaDate);
+
+        // for each dog, create a charge, times the number of days
+        for (var i = self.Dogs.length; i >= 0; i--) {
+          charges.push({
+            Quantity: deltaDate
+
+          });
+        }
+
+        // create an invoice from the booking
+        /*Invoice.create({
+          Date: new Date(),
+          Job: result.id,
+          Charges:
+        })*/
+      }
+    })
+  }
+};
--- Server/api/models/Jobtype.js
+++ Server/api/models/Jobtype.js
@@ -1,24 +1,31 @@
 /**
-* Jobtype.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+ * Jobtype.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/#!documentation/models
+ */
 
 module.exports = {
 
   attributes: {
-      Name: {
-          type: 'string',
-          required: true
-      },
-      Description:  {
-          type: 'string' 
-      },
-      Jobs: {
-          collection: 'job',
-          via: 'Jobtype'
-      }
+    Name: {
+      type: 'string',
+      required: true
+    },
+    Description: {
+      type: 'string'
+    },
+    Jobs: {
+      collection: 'job',
+      via: 'Jobtype'
+    },
+    Value: {
+      type: 'float',
+      required: true
+    },
+    Charges: {
+      collection: 'charge',
+      via: 'JobType'
+    }
   }
 };
-
