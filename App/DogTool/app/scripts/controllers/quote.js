75a797cc2b561692622a052b7cae8f65722c0d55
timo0060 <timo0060@gmail.com>
2015-07-14 21:16:22 -0400

    Uploading files for Jean-Luc to view Errors in Build

--- App/DogTool/app/scripts/controllers/quote.js
+++ App/DogTool/app/scripts/controllers/quote.js
@@ -8,53 +8,46 @@
  * Controller of the dogToolApp
  */
 angular.module('dogToolApp')
-  .controller('QuoteCtrl', function ($scope, FactoryBusinessInfo, FactoryQuote, flash) {
-   
-  //--------------------------------------Business Info Stuff------------------------------------------------------
+  .controller('QuoteCtrl', function ($scope, FactoryBusinessInfo, FactoryQuote, FactoryService, flash) {
 
-  FactoryBusinessInfo.get()
-    .success(function (res) {
-      $scope.BusinessInfo = res;
-    });
-  
-  $scope.Date = new Date(); 
-  
-  //-----------------------------------Adding Services---------------------------------------------------------
-  $scope.addingService = false;
-  $scope.Services = {};
-  
-  $scope.ServiceList = [
-    {
-        Name: "Daycare",
-        Value: 50.99
-    },
-    {
-        Name: "Bath",
-        Value: 25.99
-    }
-  ];
-    
-  $scope.addService = function(){
-    $scope.addingService = true;
-  };
-  
-  $scope.cancelService = function(){
+    //--------------------------------------Business Info Stuff------------------------------------------------------
+
+    FactoryBusinessInfo.get()
+      .success(function (res) {
+        $scope.BusinessInfo = res;
+      });
+
+
+    $scope.Date = new Date();
+
+    //-----------------------------------Adding Services---------------------------------------------------------
     $scope.addingService = false;
-    $scope.Services = null;
-    $scope.AddServiceForm.$setUntouched();
-  };
-  
-  $scope.saveQuote = function(){
-    alert("Saved");
-  }
-  
-  $scope.saveService = function(){
-    $scope.cancelService();
-  }
-  
-});
+
+    FactoryService.getAll()
+      .success(function (res) {
+        $scope.ServiceList = res;
+      });
 
 
 
+    $scope.Services = {};
 
+    $scope.addService = function () {
+      $scope.addingService = true;
+    };
 
+    $scope.cancelService = function () {
+      $scope.addingService = false;
+      $scope.Services = null;
+      $scope.AddServiceForm.$setUntouched();
+    };
+
+    $scope.saveQuote = function () {
+      alert("Saved");
+    }
+
+    $scope.saveService = function () {
+      $scope.cancelService();
+    }
+
+  });
--- App/DogTool/app/scripts/services/sailsroute.js
+++ App/DogTool/app/scripts/services/sailsroute.js
@@ -12,11 +12,11 @@ angular.module('dogToolApp')
 
     var buildRoutes = function (objectName) {
       return {
-        route:  SAILS_URL + '/' + objectName,
+        route: SAILS_URL + '/' + objectName,
         getAll: SAILS_URL + '/' + objectName,
         listen: SAILS_URL + '/' + objectName,
-        post:   SAILS_URL + '/' + objectName,
-        find:   SAILS_URL + '/' + objectName + '/find',
+        post: SAILS_URL + '/' + objectName,
+        find: SAILS_URL + '/' + objectName + '/find',
         get: function (id) {
           return this.route + '/' + id;
         },
@@ -27,21 +27,22 @@ angular.module('dogToolApp')
     };
 
     return {
-      Dog:          buildRoutes('dog'),
-      Cost:         buildRoutes('cost'),
-      Calendar:     buildRoutes('calendar'),
-      Weight:       buildRoutes('weight'),
-      Daycare:      buildRoutes('daycare'),
-      Note:         buildRoutes('note'),
-      People:       buildRoutes('people'),
-      Homework:     buildRoutes('homework'),
-      Address:      buildRoutes('address'),
+      Dog: buildRoutes('dog'),
+      Cost: buildRoutes('cost'),
+      Calendar: buildRoutes('calendar'),
+      Weight: buildRoutes('weight'),
+      Daycare: buildRoutes('daycare'),
+      Note: buildRoutes('note'),
+      People: buildRoutes('people'),
+      Homework: buildRoutes('homework'),
+      Address: buildRoutes('address'),
       Consultation: buildRoutes('consultation'),
-      Job:          buildRoutes('job'),
+      Job: buildRoutes('job'),
 
       BusinessInfo: buildRoutes('businessinfo'),
-      Expense:      buildRoutes('expense'),
-      JobType:      buildRoutes('jobtype')
+      Expense: buildRoutes('expense'),
+      JobType: buildRoutes('jobtype'),
+      Service: buildRoutes('service')
 
     };
   });
