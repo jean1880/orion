6c5bba4372ba8e69015a136da40b7a4c9d49ddf1
Jean-Luc Desroches <jean18801880@gmail.com>
2015-07-14 21:59:57 -0400

    Fix missing quote route

---
+++ Server/api/controllers/QuoteController.js
@@ -0,0 +1,11 @@
+/**
+ * QuoteController
+ *
+ * @description :: Server-side logic for managing quotes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
